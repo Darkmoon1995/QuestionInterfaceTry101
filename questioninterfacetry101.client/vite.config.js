96a8fd6e17412171e3a722c17da6140cf8fb2767
Darkmoon <Darkmoonmasih@gmail.com>
2024-09-07 19:12:43 +0330

    ViteConfig Updated a littile

--- questioninterfacetry101.client/vite.config.js
+++ questioninterfacetry101.client/vite.config.js
@@ -33,6 +33,8 @@ if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
     env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7226';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [plugin()],
@@ -61,7 +63,7 @@ export default defineConfig({
             }
         },
         port: 5173,
-        https: {
+        https: !isProd && {
             key: fs.readFileSync(keyFilePath),
             cert: fs.readFileSync(certFilePath),
         }
