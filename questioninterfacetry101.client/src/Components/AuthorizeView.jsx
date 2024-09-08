102b3d98d320e2ec93004c7102e2ce8b891f145d
Darkmoon <Darkmoonmasih@gmail.com>
2024-09-08 18:51:27 +0330

    Update23456789

--- questioninterfacetry101.client/src/Components/AuthorizeView.jsx
+++ questioninterfacetry101.client/src/Components/AuthorizeView.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, createContext } from 'react';
 import { Navigate } from 'react-router-dom';
-import axiosInstance from '../Components/axiosConfig';
 
 const UserContext = createContext({});
 
